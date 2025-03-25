70c2d5fbac55b0d08989764d15f8c83922700065
Carlos Escobar <carlosgem30@gmail.com>
2025-03-25 11:46:40 -0500

    refactor: Changed the logic for generating new IDs. Instead of using metadata, it now uses a TreeMap

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,49 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         File file = new File("tasks.json");
-        HashMap<Integer,Task> tasksHashMap = new HashMap<>(Formatters.formatToTasks(file));
-        System.out.println("Ingrese el id de la tarea que quiere eliminar:");
-        Integer taskId = scanner.nextInt();
-        /*TaskManager.deleteTask(taskId,file);*/
+        System.out.println("que desea hacer?");
+        System.out.println("1.Crear tarea");
+        System.out.println("2.actualizar tarea");
+        System.out.println("3.eliminar tarea");
+        String option = scanner.nextLine();
+        actions:switch (option){
+            case "1":
+                System.out.println("por favor, ingrese la nueva tarea");
+                String taskDescription = scanner.nextLine();
+                if (!file.exists()){
+                    CreateTasks.createFirstTasks(taskDescription);
+                }else {
+                    CreateTasks.createNextTask(taskDescription,file);
+                }
+                System.out.println("Tarea creada con exito");
+                break;
+
+            case "2":
+                System.out.println("¿que desea actualizar?");
+                System.out.println("1.descripcion");
+                System.out.println("2.estado");
+                String optionUpdate = scanner.nextLine();
+                System.out.println("por favor ingrese el id del elemento que desea modificar");
+                String taskIdString = scanner.nextLine();
+                Integer taskId = Integer.parseInt(taskIdString);
+                switch (optionUpdate){
+                    case "1":
+                        System.out.println("por favor ingrese la nueva descripcion");
+                        String newTaskDescription = scanner.nextLine();
+                        TaskManager.updateDescription(taskId,newTaskDescription,file);
+                        System.out.println("Descripcion actualizada con exito");
+                        break actions;
+                    case "2":
+                        System.out.println("por favor ingrese el nuevo estado");
+                        String newTaskStatus = scanner.nextLine();
+                        TaskManager.updateStatus(taskId,newTaskStatus,file);
+                        System.out.println("Estado actualizada con exito");
+                        break actions;
+                    case "3":
+
+                }
+
+        }
 
     }
 }
